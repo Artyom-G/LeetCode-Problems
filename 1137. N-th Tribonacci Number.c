46e5de52d4715333620db09f06665d667c6a253d
Artyom Gabtraupov <58640306+Artyom-G@users.noreply.github.com>
2024-04-24 14:33:32 -0400

    Create 1137. N-th Tribonacci Number.c

---
+++ 1137. N-th Tribonacci Number.c
@@ -0,0 +1,16 @@
+int tribonacci(int n){
+    if(n == 0){
+        return 0;
+    }
+    int t0 = 1;
+    int t1 = 1;
+    int t2 = 1; 
+    int t3 = 0;
+    for(int i = 2; i < n; i++){
+        t0 = t1 + t2 + t3; 
+        t3 = t2;
+        t2 = t1;
+        t1 = t0;
+    }
+    return t0;
+}
