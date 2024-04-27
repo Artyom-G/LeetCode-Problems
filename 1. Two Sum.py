1e22b144dd032e9084b02203cf4c8bd3d3312973
Artyom Gabtraupov <58640306+Artyom-G@users.noreply.github.com>
2024-04-27 02:15:02 -0400

    Update 1. Two Sum.py

--- 1. Two Sum.py
+++ 1. Two Sum.py
@@ -1,4 +1,6 @@
-from collections import defaultdict 
+#Time Complexity O(n)
+#Space Complexity O(n)
+#Approach: Hash Map
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         dic = defaultdict(lambda: None) 
@@ -7,4 +9,34 @@ class Solution:
             if dic[second] != None:
                 return [i, dic[second]]
             dic[nums[i]] = i
+
+
+"""
+#Time Complexity O(nlogn) and O(logn) without having to sort
+#Space Complexity O(n)
+#Approach: Sorted List and Binary Search
+#Note: This does not pass LeetCode due to mismatching indicies but this is really useful for some other problems
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        nums.sort()
+        for x in range(len(nums)):
+            y = self.binary_search(nums, target - nums[x])
+            if(y != -1):
+                return [x, y]
+
+    #Binary Search: GEEKS FOR GEEKS
+    def binary_search(self, arr, x):
+        low = 0
+        high = len(arr) - 1
+        mid = 0
+        while low <= high:
+            mid = (high + low) // 2
+            if arr[mid] < x:
+                low = mid + 1
+            elif arr[mid] > x:
+                high = mid - 1
+            else:
+                return mid
+        return -1
+"""
 
