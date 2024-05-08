7bd91a0540ce6ea4744d3e6bf766698b66253b4a
Artyom Gabtraupov <58640306+Artyom-G@users.noreply.github.com>
2024-05-07 20:47:10 -0400

    Create 506. Relative Ranks.py

---
+++ 506. Relative Ranks.py
@@ -0,0 +1,22 @@
+#Time Complexity: O(n)
+#Space Complexity: O(n)
+#Approach: value-index vector pair
+class Solution:
+    def findRelativeRanks(self, score: List[int]) -> List[str]:
+        vectorPair = []
+        for i in range(len(score)):
+            vectorPair.append([score[i], i])
+        
+        vectorPair.sort(key = lambda x: -x[0])
+
+        placements = ["" for i in vectorPair]
+
+        if(len(placements) > 0):
+            placements[vectorPair[0][1]] = "Gold Medal"
+            if(len(placements) > 1): placements[vectorPair[1][1]] = "Silver Medal"
+            if(len(placements) > 2): placements[vectorPair[2][1]] = "Bronze Medal"
+
+        for i in range(3, len(vectorPair)):
+            placements[vectorPair[i][1]] = str(i + 1)
+        
+        return placements
