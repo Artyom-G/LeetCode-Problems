9d11334e9916864a380c89b88f3a233ccecb35a0
Artyom Gabtraupov <58640306+Artyom-G@users.noreply.github.com>
2024-05-28 20:41:36 -0400

    Create 1404. Number of Steps to Reduce a Number in Binary Representation to One.c

---
+++ 1404. Number of Steps to Reduce a Number in Binary Representation to One.c
@@ -0,0 +1,35 @@
+//Time Complexity: O(n)
+//Space Complexity: O(1)
+//Approach: Bit Operations
+
+int numSteps(char* s) {
+    int counter = 0;
+    int i = strlen(s) - 1;
+    bool increment = 0;
+    while(strlen(s) > 1){
+        i = strlen(s) - 1;
+        if(increment == 1){
+            increment = 0;
+        }
+        else if(s[i] == '0'){
+            s[i] = '\0';
+        }
+        else{
+            while(i > 0){
+                increment = 1;
+                if(s[i] == '0'){
+                    s[i] = '1';
+                    increment = 0;
+                    break;
+                }
+                else{
+                    s[i] = '0';
+                }
+                i--;
+            }
+        }
+        counter++;
+        printf("%s\n", s);
+    }
+    return counter;
+}
