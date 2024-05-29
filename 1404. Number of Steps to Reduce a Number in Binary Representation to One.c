51d1ca3d7510209a79eb2d59bffec5f690577d08
Artyom Gabtraupov <58640306+Artyom-G@users.noreply.github.com>
2024-05-28 20:47:27 -0400

    Update 1404. Number of Steps to Reduce a Number in Binary Representation to One.c

--- 1404. Number of Steps to Reduce a Number in Binary Representation to One.c
+++ 1404. Number of Steps to Reduce a Number in Binary Representation to One.c
@@ -8,14 +8,18 @@ int numSteps(char* s) {
     bool increment = 0;
     while(strlen(s) > 1){
         i = strlen(s) - 1;
+        //Incrementing is explained below, but essentially is an edge case
         if(increment == 1){
             increment = 0;
         }
+        //If even then divide by 2 in bin
         else if(s[i] == '0'){
             s[i] = '\0';
         }
+        //If odd then increment every bit from the end by 1 until you reach a 0 then stop incrementing
         else{
             while(i > 0){
+                //This sets the increment to 1 in the case where the input string is '111' and needs to be expanded to '1000', this carries the extra carry-on bit
                 increment = 1;
                 if(s[i] == '0'){
                     s[i] = '1';
@@ -29,7 +33,6 @@ int numSteps(char* s) {
             }
         }
         counter++;
-        printf("%s\n", s);
     }
     return counter;
 }
