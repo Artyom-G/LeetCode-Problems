5eb4a3ce18afa7056a013bcceba40e790bf6c957
Artyom Gabtraupov <58640306+Artyom-G@users.noreply.github.com>
2024-06-03 13:42:08 -0400

    Create 16. 3Sum Closest.py

---
+++ 16. 3Sum Closest.py
@@ -0,0 +1,22 @@
+#Time Complexity: O(n^2*logn)
+#Space Complexity: O(n) but could even be O(1) if different sort is used
+#Approach: 2 Pointer, Two-Sum
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        nums.sort()
+        closest = 999999999
+        for i in range(len(nums)):
+            j = i + 1
+            k = len(nums) - 1
+            while j < k:
+                S = nums[i] + nums[j] + nums[k]
+                if S < target:
+                    j+=1
+                elif S > target:
+                    k -= 1
+                else:
+                    return target
+                if abs(closest - target) > abs(S - target):
+                    closest = S
+        return closest
+                
