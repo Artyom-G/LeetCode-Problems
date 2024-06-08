9adf9b2cd392aa0a98604d46e99cc9ecfc430cb0
Artyom Gabtraupov <58640306+Artyom-G@users.noreply.github.com>
2024-06-08 11:01:05 -0400

    Update 523. Continuous Subarray Sum.py

--- 523. Continuous Subarray Sum.py
+++ 523. Continuous Subarray Sum.py
@@ -1,3 +1,22 @@
+class Solution(object):
+    def checkSubarraySum(self, nums, k):
+        remainder_index_map = {0: -1}
+        sum = 0
+        
+        for i in range(len(nums)):
+            sum += nums[i]
+            if k != 0:
+                sum %= k
+            
+            if sum in remainder_index_map:
+                if i - remainder_index_map[sum] > 1:  
+                    return True
+            else:  
+                remainder_index_map[sum] = i
+        
+        return False
+        
+
 #Time Complexity: O(n^2), Exceeds Time Limit
 #Space Complexity: O(1)
 #Approach: Brute Force
