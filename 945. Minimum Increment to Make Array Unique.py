8610a267e951045c06c6bcd4b37af0b0c3626023
Artyom Gabtraupov <58640306+Artyom-G@users.noreply.github.com>
2024-06-13 23:05:43 -0400

    Update 945. Minimum Increment to Make Array Unique.py

--- 945. Minimum Increment to Make Array Unique.py
+++ 945. Minimum Increment to Make Array Unique.py
@@ -10,22 +10,11 @@ class Solution:
             else:
                 i+=1
         
-        #print(used)
-        used = list(used)
-        #print(used)
-
         while len(nums) > 0:
-            try:
-                index = used.index(nums[-1])
-                while nums[-1] == used[index]:
-                    nums[-1] += 1
-                    index+=1
-                    counter += 1
-                used.insert(index, nums[-1])
-
-            except:
-                used.append(nums[-1])
-                used.sort()
-            nums.pop()
+            while nums[-1] in used:
+                nums[-1] += 1
+                counter += 1
+            used.add(nums[-1])
+            nums.pop(-1)
 
         return counter
