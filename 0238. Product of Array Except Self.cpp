698eb89b21de7c1ebf751fc9c907628f93efac2a
Artyom Gabtraupov <58640306+Artyom-G@users.noreply.github.com>
2024-06-16 01:31:57 -0400

    Update 0238. Product of Array Except Self.cpp

--- 0238. Product of Array Except Self.cpp
+++ 0238. Product of Array Except Self.cpp
@@ -1,11 +1,10 @@
 //Time Complexity: O(n)
-//Space Complexity: O(n)
+//Space Complexity: O(n) with O(1) Extra Space Complexity
 //Approach: Prefix-Product, Suffix-Product
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         vector<int> prefix;
-        vector<int> suffix;
         int n = nums.size();
         
         prefix.push_back(1);
@@ -13,13 +12,18 @@ public:
             prefix.push_back(prefix[i-1]*nums[i-1]);
         }
 
-        suffix.push_back(1);
-        for(int i = 1; i < n; i++){
-            suffix.push_back(suffix[i-1]*nums[n-i]);
+        //nums is the suffix
+        int previous = nums[n-1];
+        nums[n-1] = 1;
+        int current;
+        for(int i = n-2; i >= 0; i--){
+            current = nums[i];
+            nums[i] = nums[i+1] * previous;
+            previous = current;
         }
 
         for(int i = 0; i < n; i++){
-            prefix[i] *= suffix[n-i-1];
+            prefix[i] *= nums[i];
         }
 
         return prefix;
