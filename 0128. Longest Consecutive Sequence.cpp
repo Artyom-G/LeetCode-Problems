e69a7a94df1fb64c297cdda314f0c7581ff51790
Artyom Gabtraupov <58640306+Artyom-G@users.noreply.github.com>
2024-06-16 02:31:08 -0400

    Update 0128. Longest Consecutive Sequence.cpp

--- 0128. Longest Consecutive Sequence.cpp
+++ 0128. Longest Consecutive Sequence.cpp
@@ -1,4 +1,37 @@
-//Time Complexity: O(n^2)
+//Time Complexity: O(n^3) but O(n) on average
+//Space Complexity: O(n)
+//Approach: Hashmaps
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_map<int, bool> map;
+        for(int i = 0; i<nums.size(); i++){
+            map[nums[i]] = true;
+        }
+        for(int i=0; i<nums.size(); i++){
+            if(map.count(nums[i]-1) > 0){
+                map[nums[i]] = false;
+            }
+        }
+        int maxlen = 0;
+        for(int i=0; i<nums.size(); i++){
+            if(map[nums[i]] == true){
+                int j=0; int count=0;
+                while(map.count(nums[i]+j) > 0){
+                    j++;
+                    count++;
+                }
+                if(count>maxlen){
+                    maxlen = count;
+                }
+            }
+        }
+        return maxlen;
+    }
+};
+
+
+//Time Complexity: O(n^3) but O(n) on average
 //Space Complexity: O(n)
 //Approach: Sets, Hashmaps
 //Time Limit Exceeded
