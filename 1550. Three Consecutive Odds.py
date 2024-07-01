02dbe2f3c225d7311e4ca3e09c6015c667807b5d
Artyom Gabtraupov <58640306+Artyom-G@users.noreply.github.com>
2024-07-01 16:06:51 -0400

    Create 1550. Three Consecutive Odds.py

---
+++ 1550. Three Consecutive Odds.py
@@ -0,0 +1,14 @@
+#Time Complexity: O(n)
+#Space Comlplexity: O(1)
+#Approach: Array
+class Solution:
+    def threeConsecutiveOdds(self, arr: List[int]) -> bool:
+        count = 0
+        for i in arr:
+            if i % 2 == 1:
+                count += 1
+                if count >= 3:
+                    return True
+            else:
+                count = 0
+        return False
