514416f4fcf4a35229b88ced3013513c03d8ad03
Artyom Gabtraupov <58640306+Artyom-G@users.noreply.github.com>
2024-07-05 21:11:24 -0400

    Update 2582. Pass the Pillow.py

--- 2582. Pass the Pillow.py
+++ 2582. Pass the Pillow.py
@@ -1,4 +1,14 @@
 #Time Complexity: O(1)
+#Space Complexity: O(0)
+#Approach: Modular Arithmetic, Math
+class Solution:
+    def passThePillow(self, n: int, time: int) -> int:    
+        if (time // (n - 1)) % 2 == 0:
+            return (time % (n - 1)) + 1
+        else:
+            return n - (time % (n - 1))
+
+#Time Complexity: O(1)
 #Space Complexity: O(1)
 #Approach: Modular Arithmetic, Math
 class Solution:
