3d3b426ca1781b892bf7756d64c42dcc44557340
Artyom Gabtraupov <58640306+Artyom-G@users.noreply.github.com>
2024-07-13 19:06:56 -0400

    Update 2751. Robot Collisions.cpp

--- 2751. Robot Collisions.cpp
+++ 2751. Robot Collisions.cpp
@@ -1,3 +1,6 @@
+//Time Complexity: O(n^2)
+//Space Complexity: O(n)
+//Approach: Math, Stack
 class Solution {
 public:
     vector<int> survivedRobotsHealths(vector<int>& positions, vector<int>& healths, string directions) {
@@ -8,47 +11,42 @@ public:
 
         sort(robots.begin(), robots.end());
 
-        for(tuple<int, int, char> r : robots){
-            cout << get<0>(r) << ' ' << get<1>(r) << ' ' << get<2>(r) << '\n';
-        }
-        cout << '\n';
-
         while(true){
-        //Must update the positions too, the position
-        vector<tuple<int, int, char>> new_robots;
-        char previous = get<2>(robots[0]);
-        for(int i = 0; i < robots.size(); i++){
-            if(!(get<2>(robots[i]) == 'L' && previous == 'R')){
-                new_robots.push_back(robots[i]);
+            vector<tuple<int, int, char>> new_robots;
+            char previous = get<2>(robots[0]);
+            for(int i = 0; i < robots.size(); i++){
+                if(!(get<2>(robots[i]) == 'L' && previous == 'R')){
+                    new_robots.push_back(robots[i]);
+                }
+                else{
+                    new_robots.pop_back();
+                    if(get<1>(robots[i]) > get<1>(robots[i-1]) && get<1>(robots[i]) > 1){
+                        new_robots.push_back(tuple<int, int, char>{get<0>(robots[i]), get<1>(robots[i]) - 1, get<2>(robots[i])});
+                    }
+                    else if(get<1>(robots[i-1]) > get<1>(robots[i]) && get<1>(robots[i-1]) > 1){
+                        new_robots.push_back(tuple<int, int, char>{get<0>(robots[i-1]), get<1>(robots[i-1]) - 1, get<2>(robots[i-1])});
+                    }
+                }
+                previous = get<2>(robots[i]);
+            }
+
+            if(robots == new_robots){
+                unordered_map<int, int> pos_health;
+                vector<int> res;
+                for(int i = 0; i < robots.size(); i++){
+                    pos_health[get<0>(robots[i])] = i+1;
+                }
+
+                for(int i = 0; i < positions.size(); i++){
+                    if(pos_health[positions[i]]){
+                        res.push_back(get<1>(robots[pos_health[positions[i]]-1]));
+                    }
+                }
+                return res;
             }
             else{
-                new_robots.pop_back();
-                if(get<1>(robots[i]) > get<1>(robots[i-1]) && get<1>(robots[i]) > 1){
-                    new_robots.push_back(tuple<int, int, char>{get<0>(robots[i]), get<1>(robots[i]) - 1, get<2>(robots[i])});
-                }
-                else if(get<1>(robots[i-1]) > get<1>(robots[i]) && get<1>(robots[i-1]) > 1){
-                    new_robots.push_back(tuple<int, int, char>{get<0>(robots[i-1]), get<1>(robots[i-1]) - 1, get<2>(robots[i-1])});
-                }
+                robots = new_robots;
             }
-            previous = get<2>(robots[i]);
-        }
-
-        if(robots == new_robots){
-            vector<int> res;
-            for(int i = 0; i < positions.size(); i++){
-                if()
-            }
-
-            return res;
-        }
-        else{
-            robots = new_robots;
-        }
-
-        for(tuple<int, int, char> r : new_robots){
-            cout << get<0>(r) << ' ' << get<1>(r) << ' ' << get<2>(r) << '\n';
-        }
-        cout << '\n';
         }
 
         return vector<int>{};
