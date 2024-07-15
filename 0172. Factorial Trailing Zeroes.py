9526d2dd516003d1e09bcde3c40e61615c7216d1
Artyom Gabtraupov <58640306+Artyom-G@users.noreply.github.com>
2024-07-15 17:37:22 -0400

    Update 0172. Factorial Trailing Zeroes.py

--- 0172. Factorial Trailing Zeroes.py
+++ 0172. Factorial Trailing Zeroes.py
@@ -1,3 +1,11 @@
+#Time Complexity: O(1)
+#Space Complexity: O(0)
+#Approach: Modular Arithmetic, Math
+#This is better than LeetCode's Follow-Up of "Could you write a solution that works in logarithmic time complexity?"
+class Solution:
+    def trailingZeroes(self, n: int) -> int:
+        return n // 5 + n // 25 + n // 125 + n // 625 + n // 3125
+
 #Time Complexity: O(nlogn)
 #Space Complexity: O(1)
 #Approach: Modular Arithmetic, Math
