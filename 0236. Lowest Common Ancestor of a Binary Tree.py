a98bfbe695a1d010f9dc99f6f6d5fda9e2b32ecc
Artyom Gabtraupov <58640306+Artyom-G@users.noreply.github.com>
2024-07-16 14:09:21 -0400

    Update 0236. Lowest Common Ancestor of a Binary Tree.py

--- 0236. Lowest Common Ancestor of a Binary Tree.py
+++ 0236. Lowest Common Ancestor of a Binary Tree.py
@@ -1,5 +1,38 @@
 #Time Complexity: O(n)
+#Space Complexity: O(logn)
+#Approach: DFS, Recursion, Tree
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    p_found = False
+    q_found = False
+    ancestor = None
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        def recursion(root=root, ancestor=root):
+            if root:
+                if root.val == p.val:
+                    self.p_found = True
+                    if self.q_found: self.ancestor = ancestor
+                if root.val == q.val:
+                    self.q_found = True
+                    if self.p_found: self.ancestor = ancestor
+                if not (self.p_found or self.q_found): recursion(root.left, root.left)
+                else: recursion(root.left, ancestor)
+                if not (self.p_found or self.q_found): recursion(root.right, root.right)
+                else: recursion(root.right, ancestor)
+        recursion()
+        return self.ancestor
+
+
+#Time Complexity: O(n)
 #Space Complexity: O(n)
+#Approach: DFS, Recursion, Tree, Intersection, Array
 
 # Definition for a binary tree node.
 # class TreeNode:
