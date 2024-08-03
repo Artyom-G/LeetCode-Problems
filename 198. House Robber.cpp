29d357c52ef8f58cc73cd5df83128dbe8073b38e
Artyom Gabtraupov <58640306+Artyom-G@users.noreply.github.com>
2024-08-02 20:22:36 -0400

    Create 198. House Robber.cpp

---
+++ 198. House Robber.cpp
@@ -0,0 +1,17 @@
+//Time Complexity: O(n)
+//Space Complexity: O(1)
+//Approach: Dynamic Programming, Iterative Fib
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        if (nums.size() == 0) return 0;
+        int prev1 = 0;
+        int prev2 = 0;
+        for (int num : nums) {
+            int tmp = prev1;
+            prev1 = max(prev2 + num, prev1);
+            prev2 = tmp;
+        }
+        return prev1;
+    }
+};
