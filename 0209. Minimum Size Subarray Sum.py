8bc9506787d748f3c9bd7c3db34b81ba4e420e70
Artyom Gabtraupov <58640306+Artyom-G@users.noreply.github.com>
2026-06-02 00:14:42 -0400

    Update 0209. Minimum Size Subarray Sum.py

--- 0209. Minimum Size Subarray Sum.py
+++ 0209. Minimum Size Subarray Sum.py
@@ -21,4 +21,41 @@ class Solution:
         if m > len(nums):
             return 0
         return m
-                
+
+
+
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+# Approach: Sliding Window
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        l = 0
+        r = 0
+        n = len(nums)
+        current_sum = nums[l]
+        res = float(inf)
+        if(current_sum >= target):
+            return 1
+        m = 1
+        while(l < n):
+            #print(nums[l:r+1], m, current_sum, res)
+            if(current_sum < target and m < res and r < n-1):
+                r+=1
+                current_sum += nums[r]
+                m+=1
+            elif(l < r or r == n-1):
+                current_sum -= nums[l]
+                l+=1
+                m-=1
+            else:
+                current_sum -= nums[l]
+                l+=1
+                r+=1
+                current_sum += nums[r]
+                if(current_sum >= target):
+                    return 1
+            if(current_sum >= target):
+                res = min(m, res)
+        if(res == float(inf)):
+            return 0
+        return res
