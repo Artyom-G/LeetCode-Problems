0abd0848c1d3e96c065d42c1138481d208148a51
Artyom Gabtraupov <58640306+Artyom-G@users.noreply.github.com>
2026-06-02 00:19:15 -0400

    Update 0383. Ransom Note.py

--- 0383. Ransom Note.py
+++ 0383. Ransom Note.py
@@ -12,3 +12,24 @@ class Solution:
                 j+=1
                 if j >= len(ransomNote): return True
         return False
+
+
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+# Approach: Hashmap
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        letters = dict()
+        for l in magazine:
+            if(l in letters):
+                letters[l] += 1
+            else:
+                letters[l] = 1
+        
+        for l in ransomNote:
+            if(l not in letters):
+                return False
+            if(letters[l] <= 0):
+                return False
+            letters[l] -= 1
+        return True
