8f1eb0625860ce3e45e179b84c72a05e7c63d54f
Artyom Gabtraupov <58640306+Artyom-G@users.noreply.github.com>
2026-08-06 20:16:08 -0400

    Update 0637. Average of Levels in Binary Tree.py

--- 0637. Average of Levels in Binary Tree.py
+++ 0637. Average of Levels in Binary Tree.py
@@ -1,5 +1,5 @@
 #Time Complexity: O(n)
-#Space Complexity: O(n)
+#Space Complexity: O(1)
 #Approach: BFS, Binary Tree
 
 # Definition for a binary tree node.
@@ -29,3 +29,41 @@ class Solution:
             res.append(level_sum / level_count)
 
         return res
+
+
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+# Approach: BFS
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def averageOfLevels(self, root: Optional[TreeNode]) -> List[float]:
+        q = deque([root])
+        level = 0
+        next_level_node = root
+        sol = []
+        running_average = 0
+        running_count = 0
+        while len(q) != 0:
+            node = q.popleft()
+            if next_level_node == node:
+                level += 1
+                next_level_node = None
+                sol.append(running_average) 
+                running_average = 0
+                running_count = 0
+            running_average = (running_average * running_count + float(node.val)) / (running_count + 1)
+            running_count += 1
+            if node.left != None:
+                if next_level_node == None: next_level_node = node.left
+                q.append(node.left)
+            if node.right != None:
+                if next_level_node == None: next_level_node = node.right
+                q.append(node.right)
+        sol.append(running_average)
+        return sol[1:]
