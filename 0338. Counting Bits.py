3fa9760ed27eb4a5b3b400eb97544da149153c6e
Artyom Gabtraupov <58640306+Artyom-G@users.noreply.github.com>
2026-08-06 23:52:44 -0400

    Update 0338. Counting Bits.py

--- 0338. Counting Bits.py
+++ 0338. Counting Bits.py
@@ -1,15 +1,14 @@
 # Time Complexity: O(n)
-# Space Complexity: O()
-# Approach: Math
+# Space Complexity: O(1) Auxilary
+# Approach: DP, Math
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        ans = [0]
-        highest_bit = 1
-
-        for i in range(1, n + 1):
-            if i == highest_bit * 2:
-                highest_bit *= 2
-
-            ans.append(1 + ans[i - highest_bit])
-
+        if n == 0: return [0]
+        # ans[i] = 1 + dp[i-dom_bit]
+        ans = [0, 1]
+        dom_bit = 1
+        for i in range(2,n+1):
+            if(i >= dom_bit):
+                dom_bit *= 2
+            ans.append(ans[i-dom_bit] + 1)
         return ans
